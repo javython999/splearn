8dad411cd5fec517685ff44c89c527268d01763f
javython999 <infinity2544@gmail.com>
2026-09-12 14:29:50 +0900

    애그리거트와 애플리케이션 컴포넌트 의존관계, ArchUnit을 이용한 슬라이스 의존 관계 검증

--- src/test/java/com/errday/splearn/HexagonalArchitectureTest.java
+++ src/test/java/com/errday/splearn/HexagonalArchitectureTest.java
@@ -1,10 +1,20 @@
 package com.errday.splearn;
 
+import com.tngtech.archunit.core.domain.JavaClass;
 import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.domain.JavaMethodCall;
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchCondition;
+import com.tngtech.archunit.lang.ConditionEvents;
+import com.tngtech.archunit.lang.SimpleConditionEvent;
 import com.tngtech.archunit.library.Architectures;
+import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 @AnalyzeClasses(packages = "com.errday.splearn", importOptions = ImportOption.DoNotIncludeTests.class)
 public class HexagonalArchitectureTest {
@@ -21,4 +31,58 @@ public class HexagonalArchitectureTest {
                 .whereLayer("adapter").mayNotBeAccessedByAnyLayer()
                 .check(classes);
     }
+
+    @ArchTest
+    void aggregateFreeOfCycles(JavaClasses classes) {
+        SlicesRuleDefinition.slices()
+                .matching("com.errday.splearn.domain.(*)..")
+                .should().beFreeOfCycles()
+                .check(classes);
+    }
+
+    @ArchTest
+    void applicationServiceFreeOfCycles(JavaClasses classes) {
+        SlicesRuleDefinition.slices()
+                .matching("com.errday.splearn.application.(*)..")
+                .should().beFreeOfCycles()
+                .check(classes);
+    }
+
+    @ArchTest
+    void aggregateDependencies(JavaClasses classes) {
+        SlicesRuleDefinition.slices()
+                .matching("com.errday.splearn.domain.(*)..")
+                .should(onlyCallGettersOrRecordMethodsOfOtherSlices())
+                .check(classes);
+    }
+
+    private <SLICE extends Set<JavaClass>> ArchCondition<SLICE> onlyCallGettersOrRecordMethodsOfOtherSlices() {
+        return new ArchCondition<>("다른 슬라이스의 getter 또는 레코드, enum 메소드만 호출할 수 있다.") {
+            private final Set<JavaClass> classesInAnySlice = new HashSet<>();
+
+            @Override
+            public void init(Collection<SLICE> allSlice) {
+                allSlice.forEach(classesInAnySlice::addAll);
+            }
+
+            @Override
+            public void check(SLICE slice, ConditionEvents events) {
+                for (JavaClass javaClass : slice) {
+                    for (JavaMethodCall call : javaClass.getMethodCallsFromSelf()) {
+                        JavaClass targetOwner = call.getTargetOwner();
+                        if (slice.contains(targetOwner)) continue;
+                        if (!classesInAnySlice.contains(targetOwner)) continue;
+                        if (targetOwner.isRecord()) continue;
+                        if (targetOwner.isEnum()) continue;
+
+                        String methodName = call.getTarget().getName();
+                        if (methodName.startsWith("get") || methodName.startsWith("is")) continue;
+
+                        events.add(SimpleConditionEvent.violated(call, call.getDescription()));
+                    }
+                }
+            }
+        };
+    }
+
 }
